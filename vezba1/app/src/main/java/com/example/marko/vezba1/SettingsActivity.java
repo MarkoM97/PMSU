5156810d74af933cc0280a86e65eeb89146c9558
marko martonosi <marko.martonosi@gmail.com>
2018-03-27 23:15:27 +0200

    V3

--- vezba1/app/src/main/java/com/example/marko/vezba1/SettingsActivity.java
+++ vezba1/app/src/main/java/com/example/marko/vezba1/SettingsActivity.java
@@ -1,18 +1,33 @@
 package com.example.marko.vezba1;
 
+import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        Toast.makeText(this, "Personalize",Toast.LENGTH_SHORT).show();
+        //setContentView(R.layout.activity_settings);
+        //Toast.makeText(this, "Personalize",Toast.LENGTH_SHORT).show();
+
+        addPreferencesFromResource(R.xml.preferences);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onStart() {
